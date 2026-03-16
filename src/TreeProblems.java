fd785d90af76428d7ebde4555b0bb62ad6f06961
konradkelly <konradky@gmail.com>
2026-03-15 23:55:59 -0700

    implement solution for sumTree (Map) method

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -133,8 +133,16 @@ public class TreeProblems {
    Hint: There's a simple way to do this!
   */
   public static int sumTree(Map<Integer, List<Integer>> tree) {
-    return -1;
+    if (tree == null) return 0;
+   
+    int sum = 0;
+   
+    for (int key : tree.keySet()) {
+      sum += key;
+    }
+    return sum;
   }
+
 
   /*
    findRoot
