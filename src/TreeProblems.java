748adfb64e0c312a015ca50d634a40d6740e3496
konradkelly <konradky@gmail.com>
2026-03-15 23:59:02 -0700

    implement solution for maxDepth (Map) method

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -236,6 +236,26 @@ public class TreeProblems {
    Hint: Use findRoot to start. Then, make a recursive helper method.
   */
   public static int maxDepth(Map<String, List<String>> tree) {
-    return -1;
+    if (tree == null) return 0;
+   
+    String root = findRoot(tree);
+   
+    return maxDepthHelper(tree, root);
+  }
+
+
+  private static int maxDepthHelper(Map<String, List<String>> tree, String node) {
+    List<String> children = tree.get(node);
+   
+    if (children == null || children.isEmpty()) return 1;
+   
+    int max = 0;
+   
+    for (String child : children) {
+      max = Math.max(max, maxDepthHelper(tree, child));
+    }
+
+
+    return max + 1;
   }
 }
