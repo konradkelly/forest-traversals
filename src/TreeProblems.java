a2f9ad3741f4d56ea62c45b840984370828bb3ef
konradkelly <konradky@gmail.com>
2026-03-15 23:55:17 -0700

    implement solution for sumTree method

--- src/TreeProblems.java
+++ src/TreeProblems.java
@@ -98,8 +98,20 @@ public class TreeProblems {
    A null tree should return 0
   */
   public static int sumTree(Node<Integer> root) {
-    return -1;
+    if (root == null) return 0;
+
+
+    int sum = root.value;
+
+
+    for (Node<Integer> child : root.children) {
+      sum += sumTree(child);
+    }
+
+
+    return sum;
   }
+
 
   /*
    sumTree (Map Version)
